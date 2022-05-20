63dd1983ccd88068075d080d3c7a0b423e5793bb
Curtis Ellis <curtis@nextcash.tech>
2022-05-19 21:14:03 -0600

    Add protocol id marshaling.

--- pkg/golang/envelope/base/base.go
+++ pkg/golang/envelope/base/base.go
@@ -1,9 +1,13 @@
 package base
 
 import (
-	"errors"
+	"encoding/hex"
+	"fmt"
+	"strings"
 
 	"github.com/tokenized/pkg/bitcoin"
+
+	"github.com/pkg/errors"
 )
 
 var (
@@ -12,18 +16,112 @@ var (
 	ProtocolIDTokenizedTest  = ProtocolID("test.TKN")
 	ProtocolIDFlag           = ProtocolID("flag")
 	ProtocolIDUUID           = ProtocolID("uuid") // Protocol ID for Universally Unique IDentifiers
-	ProtocolIDSignedMessages = ProtocolID("S")    // Protocol ID for signed messages
-	ProtocolIDInvoices       = ProtocolID("I")    // Protocol ID for invoice negot
+	ProtocolIDSignedMessages = ProtocolID("S")    // Protocol ID for Channels signed messages
+	ProtocolIDInvoices       = ProtocolID("I")    // Protocol ID for Channels invoice negotiation
+	ProtocolIDMerkleProof    = ProtocolID("MP")   // Protocol ID for Channels merkle proofs
+	ProtocolIDReject         = ProtocolID("RJ")   // Protocol ID for Channels reject messages
+	ProtocolIDMessageID      = ProtocolID("ID")   // Protocol ID for Channels message ids
+	ProtocolIDRelationships  = ProtocolID("RS")   // Protocol ID for Channels relationship messages
+	ProtocolIDResponse       = ProtocolID("RE")   // Protocol ID for Channels response messages
 
 	ErrNotEnvelope     = errors.New("Not an envelope")
 	ErrUnknownVersion  = errors.New("Unknown version")
 	ErrInvalidEnvelope = errors.New("Invalid Envelope")
 )
 
-type ProtocolID bitcoin.Hex
-type ProtocolIDs []ProtocolID // hex is just byte slice that JSON marshals as hex
+type ProtocolID bitcoin.Hex // Hex is just a byte slice that JSON marshals as hex
+type ProtocolIDs []ProtocolID
 
 type Data struct {
 	ProtocolIDs ProtocolIDs
 	Payload     bitcoin.ScriptItems
+}
+
+func (id ProtocolID) String() string {
+	if isText(id) {
+		return string(id)
+	}
+
+	return fmt.Sprintf("0x%s", hex.EncodeToString(id))
+}
+
+// MarshalJSON converts to json.
+func (id ProtocolID) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"%s\"", id)), nil
+}
+
+// UnmarshalJSON converts from json.
+func (id *ProtocolID) UnmarshalJSON(data []byte) error {
+	l := len(data)
+	if l < 2 {
+		return errors.New("Missing quotes")
+	}
+	if data[0] != '"' || data[l-1] != '"' {
+		return errors.New("Missing quotes")
+	}
+
+	s := string(data[1 : l-1])
+	if strings.HasPrefix(s, "0x") {
+		b, err := hex.DecodeString(s[2:])
+		if err != nil {
+			return errors.Wrap(err, "hex")
+		}
+
+		*id = ProtocolID(b)
+		return nil
+	}
+
+	*id = ProtocolID(data[1 : l-1])
+	return nil
+}
+
+// MarshalText returns the text encoding of the hash.
+// Implements encoding.TextMarshaler interface.
+func (id ProtocolID) MarshalText() ([]byte, error) {
+	return []byte(id.String()), nil
+}
+
+// UnmarshalText parses a text encoded hash and sets the value of this object.
+// Implements encoding.TextUnmarshaler interface.
+func (id *ProtocolID) UnmarshalText(text []byte) error {
+	s := string(text)
+	if strings.HasPrefix(s, "0x") {
+		b, err := hex.DecodeString(s[2:])
+		if err != nil {
+			return errors.Wrap(err, "hex")
+		}
+
+		*id = ProtocolID(b)
+		return nil
+	}
+
+	*id = ProtocolID(text)
+	return nil
+}
+
+// MarshalBinary returns the binary encoding of the hash.
+// Implements encoding.BinaryMarshaler interface.
+func (id ProtocolID) MarshalBinary() ([]byte, error) {
+	return id, nil
+}
+
+// UnmarshalBinary parses a binary encoded hash and sets the value of this object.
+// Implements encoding.BinaryUnmarshaler interface.
+func (id *ProtocolID) UnmarshalBinary(data []byte) error {
+	*id = data
+	return nil
+}
+
+func isText(bs []byte) bool {
+	for _, b := range bs {
+		if b < 0x20 { // ' ' space character
+			return false
+		}
+
+		if b > 0x7e { // '~' tilde character
+			return false
+		}
+	}
+
+	return true
 }
